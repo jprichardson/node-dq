369d722ec02661ed99fb3a26519bd76d9258be3d
JP Richardson <jprichardson@gmail.com>
2015-03-19 14:57:36 -0500

    dq: using JavaScript Standard Style

--- test/dq.test.js
+++ test/dq.test.js
@@ -1,76 +1,88 @@
 var dq = require('../lib/dq.js')
-  , testutil = require('testutil')
+require('testutil')
+
+/* global beforeEach, describe, F, it, T */
+/* eslint-disable no-spaced-func */
 
 var TESTQ_NAME = 'testq'
 
-describe('dq', function() {
+describe('dq', function () {
   var Q = null
-  beforeEach(function(done) {
-    dq.delete({name: TESTQ_NAME}, function(err) {
-      dq.create({name: TESTQ_NAME}, function(err, q) {
+  beforeEach(function (done) {
+    dq.delete({name: TESTQ_NAME}, function (err) {
+      F (err)
+      dq.create({name: TESTQ_NAME}, function (err, q) {
+        F (err)
         Q = q
         done()
       })
     })
   })
-    
-  describe('+ create()', function() {
-    it('should create a Queue with input params', function(done) {
-      dq.create({name: 'someQ',}, function(err, q) {
+
+  describe('+ create()', function () {
+    it('should create a Queue with input params', function (done) {
+      dq.create({name: 'someQ'}, function (err, q) {
+        F (err)
         T (q.name === 'someQ')
         done()
       })
     })
 
-    it('should  an error if name doesnt exist', function(done) {
-      dq.create({}, function(err, q) {
+    it('should  an error if name doesnt exist', function (done) {
+      dq.create({}, function (err, q) {
         T (err)
         done()
       })
     })
   })
 
-  describe('- enq()', function() {
-    it('should enqueue a value with a priority', function(done) {
-      Q.enq('string1', function(err, res) {
+  describe('- enq()', function () {
+    it('should enqueue a value with a priority', function (done) {
+      Q.enq('string1', function (err, res) {
         F (err)
-        Q.count(function(err, res) {
+        Q.count(function (err, res) {
           F (err)
           T (res === 1)
           done()
         })
       })
     })
-    
-    it('should enqueue with only the value parameter', function(done) {
+
+    it('should enqueue with only the value parameter', function (done) {
       Q.enq('someval')
       Q.enq('anotherval')
-      setTimeout(function() {
-        Q.count(function(err, res) {
+      setTimeout(function () {
+        Q.count(function (err, res) {
+          F (err)
           T (res === 2)
           done()
         })
       }, 150)
     })
-       
-    it('should enqueue with only the value and priority parameters', function(done) {
+
+    it('should enqueue with only the value and priority parameters', function (done) {
       Q.enq('z', 20)
       Q.enq('a', 1)
-       setTimeout(function() {
-         Q.count(function(err, res) {
-          T(res === 2)
-           done()
+      setTimeout(function () {
+        Q.count(function (err, res) {
+          F (err)
+          T (res === 2)
+          done()
         })
       }, 150)
     })
   })
-    
-  describe('- count()', function() {
-    it('should count the items in the queue', function(done) {
-       Q.enq('a', function(err, res) {
-         Q.enq('b', function(err, res) {
-           Q.enq('c', function(err, res) {
-             Q.count(function(err, res) {
+
+  describe('- count()', function () {
+    it('should count the items in the queue', function (done) {
+      Q.enq('a', function (err, res) {
+        F (err)
+        Q.enq('b', function (err, res) {
+          F (err)
+          Q.enq('c', function (err, res) {
+            F (err)
+            Q.count(function (err, res) {
+              F (err)
               T (res === 3)
               done()
             })
@@ -80,18 +92,24 @@ describe('dq', function() {
     })
   })
 
-  describe('- deq()', function() {
-    it('should dequeue in proper order', function(done) {
-      Q.enq('a', function(err, res) {
-        Q.enq('b', function(err, res) {
-          Q.enq('c', function(err, res) {
-            Q.deq(function(err, res) {
+  describe('- deq()', function () {
+    it('should dequeue in proper order', function (done) {
+      Q.enq('a', function (err, res) {
+        F (err)
+        Q.enq('b', function (err, res) {
+          F (err)
+          Q.enq('c', function (err, res) {
+            F (err)
+            Q.deq(function (err, res) {
+              F (err)
               T (res === 'a')
-              Q.deq(function(err, res) {
+              Q.deq(function (err, res) {
+                F (err)
                 T (res === 'b')
-                Q.deq(function(err, res) {
+                Q.deq(function (err, res) {
+                  F (err)
                   T (res === 'c')
-                  Q.deq(function(err, res) {
+                  Q.deq(function (err, res) {
                     F (err)
                     F (res)
                     done()
@@ -103,22 +121,27 @@ describe('dq', function() {
         })
       })
     })
-    
-    it('should dequeue in proper order', function(done) {
-         Q.enq('a', 0, function(err, res) {
-           Q.enq('b', -1, function(err, res) {
-             Q.enq('c', 1, function(err, res) {
-               Q.deq(function(err, res) {
-                T(res === 'b')
-                 Q.deq(function(err, res) {
-                  T(res === 'a')
-                   Q.deq(function(err, res) {
-                    T(res === 'c')
-                     Q.deq(function(err, res) {
-                      T(err === null)
-                      T(res === void 0)
-                       done()
-                    })
+
+    it('should dequeue in proper order', function (done) {
+      Q.enq('a', 0, function (err, res) {
+        F (err)
+        Q.enq('b', -1, function (err, res) {
+          F (err)
+          Q.enq('c', 1, function (err, res) {
+            F (err)
+            Q.deq(function (err, res) {
+              F (err)
+              T (res === 'b')
+              Q.deq(function (err, res) {
+                F (err)
+                T (res === 'a')
+                Q.deq(function (err, res) {
+                  F (err)
+                  T (res === 'c')
+                  Q.deq(function (err, res) {
+                    T (err === null)
+                    T (res === void 0)
+                    done()
                   })
                 })
               })
@@ -127,12 +150,14 @@ describe('dq', function() {
         })
       })
     })
+  })
 
-  describe('- quit()', function() {
-    it('should set the hasQuit flag', function(done) {
-      dq.create({name: 'blah'}, function(err, q) {
+  describe('- quit()', function () {
+    it('should set the hasQuit flag', function (done) {
+      dq.create({name: 'blah'}, function (err, q) {
+        F (err)
         F (q.hasQuit)
-        q.quit(function() {
+        q.quit(function () {
           T (q.hasQuit)
           done()
         })
@@ -140,5 +165,3 @@ describe('dq', function() {
     })
   })
 })
-
-
