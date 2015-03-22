db0e9836c2f9e759090deeedd96aeb585438c45f
JP Richardson <jprichardson@gmail.com>
2015-03-21 22:01:11 -0500

    test/dq_enq: added some more enq() tests

---
+++ test/dq_enq.test.js
@@ -0,0 +1,50 @@
+var assert = require('assert')
+var async = require('async')
+var dq = require('../')
+require('terst')
+
+/* global describe, EQ, F, it, T */
+/* eslint-disable no-spaced-func */
+
+var TESTQ_NAME = 'testq'
+
+describe('dq', function () {
+  var q
+
+  beforeEach(function (done) {
+    dq.delete({name: TESTQ_NAME}, function () {
+       q = dq.connect({name: TESTQ_NAME})
+       done()
+    })
+  })
+
+  describe('- enq()', function () {
+    describe('> when no priority', function () {
+      it('should enq in order', function (done) {
+        var data = ['a', 'b', 'c']
+        async.forEach(data, q.enq.bind(q), function (err) {
+          F (err)
+          q.redisClient.zrange(q.key, 0, 2, function (err, res) {
+            F (err)
+            assert.deepEqual(data, res)
+            done()
+          })
+        })
+      })
+    })
+
+    describe('> when priority', function () {
+      it('should enq in order', function (done) {
+        var data = [['a', 0.5], ['b', 0.1], ['c', 0.7], ['d', 0.3]]
+        async.forEach(data, function(i, cb) { q.enq.apply(q, [i[0], i[1], cb]) }, function (err) {
+          F (err)
+          q.redisClient.zrange(q.key, 0, 3, function (err, res) {
+            F (err)
+            assert.deepEqual(res, ['b', 'd', 'a', 'c'])
+            done()
+          })
+        })
+      })
+    })
+  })
+})
