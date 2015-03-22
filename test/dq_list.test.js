07ae31dfbd961d08237eac448a5aebb6a6383c9b
JP Richardson <jprichardson@gmail.com>
2015-03-22 16:23:05 -0500

    lib/dq: added +list()

---
+++ test/dq_list.test.js
@@ -0,0 +1,29 @@
+var dq = require('../')
+require('terst')
+
+/* global describe, EQ, F, it, T */
+/* eslint-disable no-spaced-func */
+
+describe('dq', function () {
+  describe('+ list()', function () {
+    it('should list all queues on the system', function (done) {
+      var dq1 = dq.connect({name: 'testq1'})
+      var dq2 = dq.connect({name: 'testq2'})
+      var dq3 = dq.connect({name: 'testq3'})
+
+      dq1.enq('hi1')
+      dq2.enq('hi2')
+      dq3.enq('hi3')
+
+      setTimeout(function () {
+        dq.list({}, function (err, qs) {
+          if (err) return done(err)
+          T (qs.indexOf('testq1') >= 0)
+          T (qs.indexOf('testq2') >= 0)
+          T (qs.indexOf('testq3') >= 0)
+          done()
+        })
+      }, 100)
+    })
+  })
+})
